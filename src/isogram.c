905063681fc12652b996760873655eb101e92353
Seagler <fkleber@gmx.net>
2020-05-06 09:12:43 +0000

    version2

--- src/isogram.c
+++ src/isogram.c
@@ -1,7 +1,24 @@
 #include "isogram.h"
-
+#include "string.h"
 
 bool is_isogram(const char phrase[]){
 
-    return false;
+    
+    int len = strlen(phrase);
+   
+    for (int i = 0; i < len; i++) {
+        char b = phrase[i];
+        int hits = 0;
+
+        for (int j = 0; j < len; j++) {
+            if ((b == phrase[j] || b == phrase[j] - 32) && (b != ' ')){
+                hits++;
+            }
+        }
+
+        if (hits > 1){
+            return false;
+        }
+    }
+    return true;
 }
--- test/test_isogram.c
+++ test/test_isogram.c
@@ -1,5 +1,6 @@
 #include "vendor/unity.h"
 #include "../src/isogram.h"
+#include "vendor/unity_internals.h"
 #include <stdlib.h>
 
 void test_empty_string(void)
@@ -7,11 +8,33 @@ void test_empty_string(void)
    TEST_ASSERT_TRUE(is_isogram(""));
 }
 
+void test_short_isogram_word(){
+    TEST_ASSERT_TRUE(is_isogram("auto"));
+}
+
+void butter_is_no_isogram(){
+    TEST_ASSERT_FALSE(is_isogram("butter"));
+}
+
+void upperCase_and_lowerCase_should_count(){
+    TEST_ASSERT_FALSE(is_isogram("Aal"));
+}
+
+void space_should_not_count(){
+    TEST_ASSERT_TRUE(is_isogram(" Florian best "));
+}
+
+
+
 int main(void)
 {
    UnityBegin("isIsogram");
 
    RUN_TEST(test_empty_string);
+   RUN_TEST(test_short_isogram_word);
+   RUN_TEST(butter_is_no_isogram);
+   RUN_TEST(upperCase_and_lowerCase_should_count);
+   RUN_TEST(space_should_not_count);
 
    UnityEnd();
    return 0;
